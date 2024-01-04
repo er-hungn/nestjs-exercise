829a3c55c540578b87b7420ec75d464fcbd71d1c
er-hungn <hungn@tech.est-rouge.com>
2024-01-04 16:22:04 +0700

    [EX3] Create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DummyModule } from './dummy/dummy.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DummyModule, AuthModule],
+  imports: [DummyModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

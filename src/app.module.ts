70da0eb8b1adc485c7fa39625325544b1c15dac3
er-hungn <hungn@tech.est-rouge.com>
2024-01-04 14:55:30 +0700

    [EX3] Create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DummyModule } from './dummy/dummy.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DummyModule],
+  imports: [DummyModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

631bb0bca5ff50fb284df96e3ff6e6cf9467f808
er-hungn <hungn@tech.est-rouge.com>
2024-01-05 10:33:49 +0700

    [EX5] Implement LoggerService

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService {
+  logRequest(request: Request) {
+    console.log(
+      `info\t${new Date().toLocaleString('en-US')}\trequest\t${
+        request.method
+      }\t${request.url}\theaders=${JSON.stringify(
+        request.headers,
+      )}\tbody=${JSON.stringify(request.body)}`,
+    );
+  }
+
+  logResponse(request: Request, response: Response, responseBody: any) {
+    console.error(
+      `info\t${new Date().toLocaleString('en-US')}\tresponse\t${
+        request.method
+      }\t${request.url}\t${response.statusCode}\theaders=${JSON.stringify(
+        response.getHeaders(),
+      )}\tbody=${JSON.stringify(responseBody)}\tprocess-time=${
+        Date.now() - request['begin']
+      }ms`,
+    );
+  }
+}

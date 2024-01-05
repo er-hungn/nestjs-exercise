1497144a4e7cc0e95f9cefd9e1e08cb41870e5b0
er-hungn <hungn@tech.est-rouge.com>
2024-01-05 10:31:49 +0700

    [EX5] Create logger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DummyModule } from './dummy/dummy.module';
+import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [DummyModule],
+  imports: [DummyModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
